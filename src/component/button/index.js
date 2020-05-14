a08fed51a6ff621dad3263cbcaad6ac8018d5fd6
Zain UL Abdin <32035288+BhuttoZen@users.noreply.github.com>
2020-05-14 14:42:46 +0500

    .

---
+++ src/component/button/index.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react'
+
+
+
+class ShareButton extends Component{
+
+  render(){
+    return(
+      <button>
+        
+      </button>
+    )
+  }
+}
+
+
+export default ShareButton
