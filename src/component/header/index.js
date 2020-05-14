78e20768386df8397d97d1e4abb0ff093f4e17d3
Zain UL Abdin <32035288+BhuttoZen@users.noreply.github.com>
2020-05-14 09:56:14 +0500

    Header component and tests

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './styles.scss'
 import Logo from '../../assets/graphics/logo.png'
 const Header =(props)=>{
-  return(<header>
+  return(<header data-test="headerComponent">
     <div className="wrap">
       <div className="logo">
-        <img src={Logo} alt="test logo" ></img>
+        <img src={Logo} alt="test logo" data-test="logoIMG" ></img>
       </div>
     </div>
   </header>
