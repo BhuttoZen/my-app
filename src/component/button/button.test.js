7103b31818143b990b5142c1aa5fda69dc84cc61
Zain UL Abdin <32035288+BhuttoZen@users.noreply.github.com>
2020-05-16 06:03:53 +0500

    Update button.test.js

--- src/component/button/button.test.js
+++ src/component/button/button.test.js
@@ -7,19 +7,26 @@ describe('Share Button Component',()=>{
 
   describe("It renders",()=>{
     let wrapper
-    
+    let mockFunc;
     beforeEach(()=>{
+      mockFunc = jest.fn();
       const props ={
         buttonText: "Example Button Text",
-        emitEvent: ()=>{
-
-        }
+        emitEvent: mockFunc
       }
       wrapper = shallow(<ShareButton {...props}/>)
     });
     it("Should render a button " , ()=>{
       const button = findByTestAttr(wrapper,'buttonComponent');
       expect(button.length).toBe(1)
+    });
+
+    it("should emit call back",()=>{
+      const button = findByTestAttr(wrapper,'buttonComponent');
+      button.simulate('click');
+      const callback = mockFunc.mock.calls.length;
+      expect(callback).toBe(1)
     })
+
   })
 })
